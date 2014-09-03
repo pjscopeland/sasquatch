12474aa8b78dc37f963e8f32ae6e3b3661117b6d
Patrick Copeland <patrick.copeland@datacom.co.nz>
2014-09-03 16:39:01 +1200

    Colour functions

--- app/models/colour.rb
+++ app/models/colour.rb
@@ -1,2 +1,53 @@
 class Colour < ActiveRecord::Base
+  include Math
+
+  # The colour in HTML format
+  def hex
+    '#' + parts.map { |c| c.to_s(16).rjust(2, '0') }.join.upcase
+  end
+
+  def hex=(hex)
+    raise 'Invalid hex colour' unless hex =~ /^#[\dA-F]{6}$/i
+    self.red, self.green, self.blue = hex.scan(/[\dA-F]{2}/i).map { |c| c.to_i(16) }
+  end
+
+  def hue
+    (atan2(sqrt(3) * (green - blue), 2 * red - green - blue) * 180 / PI).round % 360
+  end
+
+  def saturation
+    return 0.0 if value == 0
+    (1 - normalize(parts.min) / value).round(2)
+  end
+
+  def value
+    normalize(parts.max).round(2)
+  end
+
+  def lightness
+    (normalize(parts.max + parts.min) / 2).round(2)
+  end
+
+  def intensity
+    (normalize(parts.sum) / 3).round(2)
+  end
+
+  LUMA_WEIGHTS = [0.3, 0.59, 0.11]
+  def luma
+    normalize(parts.zip(LUMA_WEIGHTS).map { |a| a.inject(:*) }.sum).round(2)
+  end
+
+  def chroma
+    normalize(parts.max - parts.min).round(2)
+  end
+
+  private
+
+  def parts
+    [red, green, blue]
+  end
+
+  def normalize(number)
+    number / 255.0
+  end
 end
--- spec/models/colour_spec.rb
+++ spec/models/colour_spec.rb
@@ -1,4 +1,146 @@
 require 'rails_helper'
 
 RSpec.describe Colour, type: :model do
+  let(   :red) { Colour.find_by_name(   'Red') }
+  let(  :blue) { Colour.find_by_name(  'Blue') }
+  let(  :grey) { Colour.find_by_name(  'Grey') }
+  let(  :pink) { Colour.find_by_name(  'Pink') }
+  let( :black) { Colour.find_by_name( 'Black') }
+  let( :brown) { Colour.find_by_name( 'Brown') }
+  let( :green) { Colour.find_by_name( 'Green') }
+  let( :white) { Colour.find_by_name( 'White') }
+  let(:orange) { Colour.find_by_name('Orange') }
+  let(:purple) { Colour.find_by_name('Purple') }
+  let(:yellow) { Colour.find_by_name('Yellow') }
+
+  it '#hex' do
+    expect(   red.hex).to eq('#FF0000')
+    expect(  blue.hex).to eq('#0000FF')
+    expect(  grey.hex).to eq('#808080')
+    expect(  pink.hex).to eq('#FFC0CB')
+    expect( black.hex).to eq('#000000')
+    expect( brown.hex).to eq('#A52A2A')
+    expect( green.hex).to eq('#008000')
+    expect( white.hex).to eq('#FFFFFF')
+    expect(orange.hex).to eq('#FFA500')
+    expect(purple.hex).to eq('#800080')
+    expect(yellow.hex).to eq('#FFFF00')
+  end
+
+  it '#hex=(invalid)' do
+    expect { red.hex = '' }.to raise_error('Invalid hex colour')
+    expect { red.hex = '#AAA' }.to raise_error('Invalid hex colour')
+    expect { red.hex = '#GGGGGG' }.to raise_error('Invalid hex colour')
+    expect { red.hex = '#AAAAAAA' }.to raise_error('Invalid hex colour')
+    expect { red.hex = 'AAAAAA' }.to raise_error('Invalid hex colour')
+    expect(red.hex).to eq('#FF0000')
+  end
+
+  it '#hex=' do
+    red.hex = '#123456'
+    expect([red.red, red.green, red.blue]).to eq([18, 52, 86])
+  end
+
+  it '#hex=' do
+    red.hex = '#AbCdEf'
+    expect([red.red, red.green, red.blue]).to eq([171, 205, 239])
+  end
+
+  it '#hue' do
+    expect(   red.hue).to eq(  0)
+    expect(  blue.hue).to eq(240)
+    expect(  grey.hue).to eq(  0)
+    expect(  pink.hue).to eq(351)
+    expect( black.hue).to eq(  0)
+    expect( brown.hue).to eq(  0)
+    expect( green.hue).to eq(120)
+    expect( white.hue).to eq(  0)
+    expect(orange.hue).to eq( 40)
+    expect(purple.hue).to eq(300)
+    expect(yellow.hue).to eq( 60)
+  end
+
+  it '#saturation' do
+    expect(   red.saturation).to eq(1.00)
+    expect(  blue.saturation).to eq(1.00)
+    expect(  grey.saturation).to eq(0.00)
+    expect(  pink.saturation).to eq(0.25)
+    expect( black.saturation).to eq(0.00)
+    expect( brown.saturation).to eq(0.75)
+    expect( green.saturation).to eq(1.00)
+    expect( white.saturation).to eq(0.00)
+    expect(orange.saturation).to eq(1.00)
+    expect(purple.saturation).to eq(1.00)
+    expect(yellow.saturation).to eq(1.00)
+  end
+
+  it '#value' do
+    expect(   red.value).to eq(1.00)
+    expect(  blue.value).to eq(1.00)
+    expect(  grey.value).to eq(0.50)
+    expect(  pink.value).to eq(1.00)
+    expect( black.value).to eq(0.00)
+    expect( brown.value).to eq(0.65)
+    expect( green.value).to eq(0.50)
+    expect( white.value).to eq(1.00)
+    expect(orange.value).to eq(1.00)
+    expect(purple.value).to eq(0.50)
+    expect(yellow.value).to eq(1.00)
+  end
+
+  it '#lightness' do
+    expect(   red.lightness).to eq(0.50)
+    expect(  blue.lightness).to eq(0.50)
+    expect(  grey.lightness).to eq(0.50)
+    expect(  pink.lightness).to eq(0.88)
+    expect( black.lightness).to eq(0.00)
+    expect( brown.lightness).to eq(0.41)
+    expect( green.lightness).to eq(0.25)
+    expect( white.lightness).to eq(1.00)
+    expect(orange.lightness).to eq(0.50)
+    expect(purple.lightness).to eq(0.25)
+    expect(yellow.lightness).to eq(0.50)
+  end
+
+  it '#intensity' do
+    expect(   red.intensity).to eq(0.33)
+    expect(  blue.intensity).to eq(0.33)
+    expect(  grey.intensity).to eq(0.50)
+    expect(  pink.intensity).to eq(0.85)
+    expect( black.intensity).to eq(0.00)
+    expect( brown.intensity).to eq(0.33)
+    expect( green.intensity).to eq(0.17)
+    expect( white.intensity).to eq(1.00)
+    expect(orange.intensity).to eq(0.55)
+    expect(purple.intensity).to eq(0.33)
+    expect(yellow.intensity).to eq(0.67)
+  end
+
+  it '#luma' do
+    expect(   red.luma).to eq(0.30)
+    expect(  blue.luma).to eq(0.11)
+    expect(  grey.luma).to eq(0.50)
+    expect(  pink.luma).to eq(0.83)
+    expect( black.luma).to eq(0.00)
+    expect( brown.luma).to eq(0.31)
+    expect( green.luma).to eq(0.30)
+    expect( white.luma).to eq(1.00)
+    expect(orange.luma).to eq(0.68)
+    expect(purple.luma).to eq(0.21)
+    expect(yellow.luma).to eq(0.89)
+  end
+
+  it '#chroma' do
+    expect(   red.chroma).to eq(1.00)
+    expect(  blue.chroma).to eq(1.00)
+    expect(  grey.chroma).to eq(0.00)
+    expect(  pink.chroma).to eq(0.25)
+    expect( black.chroma).to eq(0.00)
+    expect( brown.chroma).to eq(0.48)
+    expect( green.chroma).to eq(0.50)
+    expect( white.chroma).to eq(0.00)
+    expect(orange.chroma).to eq(1.00)
+    expect(purple.chroma).to eq(0.50)
+    expect(yellow.chroma).to eq(1.00)
+  end
 end
